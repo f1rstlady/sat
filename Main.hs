956f161874ee2cc9cff4718394cbd62f99138ea7
Benedikt Rips <benedikt.rips@gmail.com>
2022-01-10 21:53:48 +0100

    Feat: enable latex output via an option

--- Main.hs
+++ Main.hs
@@ -10,17 +10,22 @@ import           Prelude              hiding (log)
 import           CNF
 import           DPLL
 import           Options
+import           Printer.Latex
 
 main :: IO ()
 main = do
   opts <- execParser optionsInfo
-  let (sat, log) = runWriter $ dpll f1
-  when (listSteps opts) (print log)
+  let format :: (Show a, ToLatex a) => a -> String
+      format = case outputFormat opts of
+        Text  -> show
+        Latex -> toLatex
+      (sat, log) = runWriter $ dpll f1
+  when (listSteps opts) (putStrLn $ format log)
   if sat
      then do
        putStrLn "The formula is satisfiable."
        when (showSolution opts) $
-         let sol = show (solution log)
+         let sol = format (solution log)
           in putStrLn $ "A solution of the formula is " ++ sol ++ "."
      else putStrLn "The formula is not satisfiable."
 
--- Options.hs
+++ Options.hs
@@ -1,23 +1,44 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Options
   ( Options (..)
+  , OutputFormat (..)
   , optionsInfo
   ) where
 
-import           Options.Applicative (Parser, ParserInfo, fullDesc, header,
-                                      help, helper, info, long, progDesc, short,
-                                      switch)
+import           Options.Applicative (Parser, ParserInfo, eitherReader,
+                                      fullDesc, header, help, helper, info,
+                                      long, metavar, option, progDesc, short,
+                                      switch, value)
 
+-- The options
 data Options = Options
   { listSteps    :: Bool
+  , outputFormat :: OutputFormat
   , showSolution :: Bool
   }
 
+-- The output format.
+data OutputFormat = Text | Latex
+
 options :: Parser Options
-options = Options <$> _listSteps <*> _showSolution where
+options = Options <$> _listSteps <*> _outputFormat <*> _showSolution where
   _listSteps = switch $
        long "list-steps"
     <> short 'l'
     <> help "List the steps of the DPLL algorithm execution."
+  _outputFormat = option parseOutputFormat $
+       long "output-format"
+    <> short 'o'
+    <> metavar "FORMAT"
+    <> value Text
+    <> help ( "The output format.  Currently supported: text and latex.  " ++
+              "Defaults to text." )
+    where
+      parseOutputFormat = eitherReader $ \ case
+        "text"  -> Right Text
+        "latex" -> Right Latex
+        fmt     -> Left $ "unkown output format: " ++ fmt
   _showSolution = switch $
        long "show-solution"
     <> short 's'
